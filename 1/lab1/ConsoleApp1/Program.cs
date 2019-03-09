2624d853504627547b1d633117ef188e548036d7
farkhutdinovq <d.farkhutdinov@g.nsu.ru>
2019-03-09 17:37:16 +0700

    lab1 & lab2

---
+++ 1/lab1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = Console.ReadLine();
+            if (!DateTime.TryParse(date, out var enteredDate))
+            {
+                Console.WriteLine("Bad input");
+                return;
+            } 
+            var current = new DateTime(enteredDate.Year, enteredDate.Month, 1);
+           
+            var workingDays = 0;
+            Console.WriteLine("Mon Tue Wed Thu Fri Sat Sun");
+            if (current.DayOfWeek != DayOfWeek.Monday)
+            {
+                makeOffsets(current.DayOfWeek);
+            }
+            while (current.Month == enteredDate.Month)
+            {
+                if (current.DayOfWeek != DayOfWeek.Saturday && current.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    workingDays++;
+                }
+                Console.Write($"{current.Day, -4}");
+                if (current.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    Console.WriteLine();
+                }
+                current = current.AddDays(1);
+            }
+            Console.WriteLine("\nWorking days: {0}", workingDays);
+            Console.ReadKey();
+        }
+
+        static void makeOffsets(DayOfWeek dayOfWeek)
+        {
+            DayOfWeek day = DayOfWeek.Monday;
+            while (day.GetHashCode() != dayOfWeek.GetHashCode())
+            {
+                Console.Write("    ");
+                if (day == DayOfWeek.Saturday) break;
+                day++;
+            }
+        }
+    }
+}
---
+++ 2/lab2/lab2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.AccessControl;
+
+namespace lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String[] phrases = new[] { "Please, don't give up, ", "What doesn't kill you makes you stronger, ",
+                "Come on, dude, come on, ", "One more try, dear " };
+            List<String> history = new List<string>();
+
+            Console.WriteLine("What's your name?");
+            String name = Console.ReadLine();
+            Random rand = new Random();
+            int answer = rand.Next(51);
+
+            Console.WriteLine("Ok, " + name + ", can you guess my number from 0 to 50?");
+            //Console.WriteLine(answer);
+
+            DateTime start = DateTime.Now;
+            int counter = 1;
+            int number = 0;
+            String s = Console.ReadLine();
+            
+                try
+                {
+                    number = Int32.Parse(s);
+                    while (answer != number)
+                    {
+                        if (answer > number)
+                        {
+                            Console.WriteLine("My number is bigger");
+                            history.Add(s + " less");
+                        }
+                        else if (answer < number)
+                        {
+                            Console.WriteLine("My number is less");
+                            history.Add(s + " bigger");
+                        }
+
+                        if (counter % 4 == 0)
+                        {
+                            int phraseNext = rand.Next(phrases.Length);
+                            Console.WriteLine(phrases[phraseNext] + name);
+                        }
+
+                        s = Console.ReadLine();
+                        if (s.Equals("q"))
+                        {
+                            Console.WriteLine("See you soon");
+                            break;
+                        }
+                        number = Int32.Parse(s);
+                        counter++;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Number expected");
+                }
+
+                if (answer == number)
+                {
+                    DateTime finish = DateTime.Now;
+                    TimeSpan interval = finish - start;
+                    Console.WriteLine("Easy win");
+                    history.Add(s + " is equal");
+                    Console.WriteLine("You used " + counter + " attempts");
+                    foreach (String current in history)
+                    {
+                        Console.WriteLine(current);
+                    }
+                Console.WriteLine("You've spent " + interval.ToString());
+                }
+            }
+        
+    }
+}
